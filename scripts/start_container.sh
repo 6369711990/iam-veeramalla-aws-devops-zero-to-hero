baa9077fab9498ad1a6994468f1ffb54a8f662e2
6369711990 <169022181+6369711990@users.noreply.github.com>
2024-05-22 02:06:28 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,9 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull sabarinatha5/simple-python-flask-app
 
+# Stop and remove any existing container with the same name (if exists)
+docker stop my-flask-app || true
+docker rm my-flask-app || true
+
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sabarinatha5/simple-python-flask-app
+docker run -d -p 5000:5000 --name my-flask-app sabarinatha5/simple-python-flask-app
